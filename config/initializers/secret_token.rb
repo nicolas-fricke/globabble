e7ce6923ed54c3ba78d815293e11ebbcebd02290
Nicolas Fricke <nicolas.fricke@student.hpi.uni-potsdam.de>
2012-10-27 17:23:10 +0200

    Initial commit, create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Globabble::Application.config.secret_token = 'c4d6e68f5371f1e0b91740bb780d700076583eea06b950f6907deb1eabaa202af553cbc701531fd47ff62e0470018788b01b34108c4f052eb6ff2895daa5a5b9'
