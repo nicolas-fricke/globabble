e7ac03d3012bff7f9531dc92cba5d5367abdb27c
Anita <itzie@gmx.net>
2012-10-31 17:21:40 +0100

    add has_many and belongs_to conditions to user, place, post and blog (blog has none)

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,7 @@
 class Place < ActiveRecord::Base
   attr_accessible :description, :latitude, :longitude, :name, :url
+
+  has_many :posts
+  has_many :comments
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
+  attr_accessible :description, :latitude, :longitude, :title
+
   belongs_to :user
   belongs_to :place
-  attr_accessible :description, :latitude, :longitude, :title
+  has_many :comments
 end
